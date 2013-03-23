b867fae30fb3d5d1fa4873e490d40058fd100491
Marshall Shen <mshen@groupon.com>
2013-03-23 18:56:59 -0500

    initial release

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Introduce Zoey, a small todo command tool for Rubyists.
 
 ###Via `curl`
 ```
-curl -L https://github.com/marshallshen/zoey/config/install.sh | sh
+curl -L curl -L https://raw.github.com/marshallshen/zoey/master/config/install.sh | sh
 ```
-
 After the set up, paste the follow to the bash file of your choice (.bashrc, .zshrc, etc)
 ```
 alias zoey="~/./.zoey/lib/zoey.rb"
 ```
+
 ##Manual Install
 ```
 git clone git://github.com/marshallshen/zoey.git ~/.zoey
@@ -22,4 +22,43 @@ chmod +x ./zoey/lib/zoey.rb
 After the set up, paste the follow to the bash file of your choice (.bashrc, .zshrc, etc)
 ```
 alias zoey="~/./.zoey/lib/zoey.rb"
+```
+
+##Usage
+Inside your terminal, you add ask for help!
+```
+> zoey help
+
+Usage: todo [options] COMMAND
+
+Commands:
+  add TODO             Adds a todo
+  check NUM            Removes a todo
+  list ls [CONTEXT]    Lists all active todos
+```
+
+Manage your todo list
+```
+> zoey add write a blog post
+Added: write a blog post: @next
+
+> zoey add work out at 6 @health
+Added: work out at 6: @health
+
+> zoey add hang out with Marshall and Lily @routine
+> Added: hang out with Marshall and Lily: @routine
+
+> zoey ls
+1: write a blog post                    @next
+2: work out at 6                        @health
+3: hang out with Marshall and Lily      @routine
+
+> zoey check 1
+Check: write a blog post: @done
+```
+
+Advanced management of todos
+```
+> zoey ls @health
+1: work out at 6      @health
 ```
--- config/install.sh
+++ config/install.sh
@@ -13,37 +13,11 @@ hash git >/dev/null && /usr/bin/env git clone https://github.com/marshallshen/zo
 echo "\033[0;34mInstalling Zoey...\033[0m"
 if [-d ~/.zoey]
 then
-  filename = "~/.zoey/lib/zoey.rb"
+  filename= "~/.zoey/lib/zoey.rb"
   chmod +x "$filename"
   echo "Zoey installed"
   exit
 fi
-
-# add zoey alias
-# alias zoey="./~/./zoey/lib/zoey.rb"
-
-echo "\033[0;34mLooking for an existing zsh config...\033[0m"
-if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]
-then
-  echo "\033[0;33mFound ~/.zshrc.\033[0m \033[0;32mBacking up to ~/.zshrc.pre-zoey\033[0m";
-  mv ~/.zshrc ~/.zshrc.pre-zoey;
-fi
-
-echo "\033[0;34mUsing the Zoey template file and adding it to ~/.zshrc\033[0m"
-cp ~/.zoey/templates/zshrc.zsh-template ~/.zshrc
-
-echo "\033[0;34mCopying your current PATH and adding it to the end of ~/.zshrc for you.\033[0m"
-echo "export PATH=$PATH" >> ~/.zshrc
-
-echo "\033[0;34mTime to change your default shell to zsh!\033[0m"
-chsh -s `which zsh`
-
-# echo "\033[0;32m"'         __                                     __   '"\033[0m"
-# echo "\033[0;32m"'  ____  / /_     ____ ___  __  __   ____  _____/ /_  '"\033[0m"
-# echo "\033[0;32m"' / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \ '"\033[0m"
-# echo "\033[0;32m"'/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / / '"\033[0m"
-# echo "\033[0;32m"'\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/  '"\033[0m"
-# echo "\033[0;32m"'                        /____/                       '"\033[0m"
 
 echo "\033[0;32m"' _______ _______ _______         '"\033[0m"
 echo "\033[0;32m"'/ ___   |  ___  |  ____ \\     /|'"\033[0m"
@@ -55,5 +29,3 @@ echo "\033[0;32m"' /   (_/\ (___) | (____/\  | |   '"\033[0m"
 echo "\033[0;32m"'(_______(_______|_______/  \_/   '"\033[0m"
 
 echo "\n\n \033[0;32m....is now installed.\033[0m"
-/usr/bin/env zsh
-source ~/.zshrc
